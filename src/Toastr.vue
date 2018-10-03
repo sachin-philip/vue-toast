361518e0c04fe05132060e4dc9b84ef3262c290d
Sachin Philip Mathew <me@imsach.in>
2018-10-04 00:25:33 +0530

    initial commit

---
+++ src/Toastr.vue
@@ -0,0 +1,112 @@
+<template>
+    <transition :enter-active-class="enterActiveClass" :leave-active-class="leaveActiveClass" @before-enter="beforeEnter" @after-enter="afterEnter" @before-leave="beforeLeave">
+        <div class="notification" v-bind:class="'is-success'+type" v-if="show">
+            <button class="delete" role="button" @click="hideToastr" v-if="closeButton"></button>
+            {{message}}
+        </div>
+    </transition>
+</template>
+
+<script>
+
+export default {
+    name: 'Toastr',
+    data: () => {
+        return {
+            show: false
+        }
+    },
+    props: {
+        type: {
+            type: String,
+            default: 'success'
+        },
+        position: {
+            type: String,
+            default: 'bottom right'
+        },
+        message: {
+            type: String
+        },
+        closeButton: {
+            type: Boolean,
+            default: true
+        },
+        icon: {
+            type: String
+        },
+        timeOut: {
+            default: '1500'
+        },
+        showMethod: {
+            type: String,
+            default: 'fadeIn'
+        },
+        hideMethod: {
+            type: String,
+            default: 'fadeOut'
+        },
+        showDuration: {
+            default: '2000'
+        },
+        hideDuration: {
+            default: '1000'
+        },
+        delay: {
+            default: '0'
+        }
+    },
+    beforeMount() {
+        let toastContainer = document.querySelector(`.bulma-toastr-container.toast-${this.positionClass}`)
+        if (!toastContainer) {
+            toastContainer = document.createElement('div')
+
+            toastContainer.classList.add('bulma-toastr-container')
+            toastContainer.classList.add(`toast-${this.positionClass}`)
+            document.body.appendChild(toastContainer)
+        }
+        toastContainer.appendChild(this.$el)
+    },
+    mounted() {
+        setTimeout(() => this.showToastr(), this.delay)
+    },
+    computed: {
+        positionClass() {
+            return this.position.split(' ').join('-')
+        },
+        enterActiveClass() {
+            return 'animated ' + this.showMethod
+        },
+        leaveActiveClass() {
+            return 'animated ' + this.hideMethod
+        },
+        
+    },
+    methods: {
+        showToastr() {
+            this.show = true
+            this.sto = setTimeout(() => this.hideToastr(), this.timeOut)
+        },
+        hideToastr() {
+            clearTimeout(this.sto)
+            clearTimeout(this.progress.intervalId)
+            this.show = false
+        },
+        beforeEnter(el) {
+            el.style.animationDuration = this.showDuration + 'ms'
+        },
+        afterEnter(el) {
+            this.$el.classList.add('animated')
+            this.$el.classList.add(this.showMethod)
+        },
+        beforeLeave(el) {
+            el.style.animationDuration = this.hideDuration + 'ms'
+        }
+    }
+}
+
+</script>
+<style lang='sass'>
+@import './assets/style.sass'
+</style>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,50 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Toastr from './Toastr'
+import '@/assets/toast.css'
+import 'animate.css/animate.css'
 
-Vue.config.productionTip = false
+const BulmaToastr = {
+    install: function (Vue, options) {
+        if (!options) {
+            options = {}
+        }
+        let showedToastrs = []
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+        function showToast(obj, type) {
+            const ToastrComponent = Vue.extend(Toastr)
+            var propsData = Object.assign(options, obj, {
+                type: type
+            })
+            let component = new ToastrComponent({
+                el: document.createElement('div'),
+                propsData
+            })
+
+            showedToastrs.push(component)
+
+            return component
+        }
+
+        Vue.prototype.$toast = {
+            success(obj) {
+                return showToast(obj, 'success')
+            },
+            info(obj) {
+                return showToast(obj, 'info')
+            },
+            warn(obj) {
+                return showToast(obj, 'warning')
+            },
+            error(obj) {
+                return showToast(obj, 'danger')
+            },
+            removeAll() {
+                showedToastrs.forEach(c => {
+                    c.hideToastr()
+                })
+                showedToastrs = []
+            }
+        }
+    }
+}
+
+export default BulmaToastr
